822ade8bb86834dd2a40832bd37e00e3477cf669
yuribertoli <yuribertoli@yahoo.it>
2022-03-03 18:21:10 +0100

    blog posts

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -5,6 +5,7 @@
       <ServicesPart/>
       <GroomingProducts/>
       <ReviewsPart/>
+      <RecentBlogPosts/>
   </main>
 </template>
 
@@ -12,8 +13,9 @@
 import MyJumbotron from "./partialsMain/MyJumbotron.vue";
 import ProBarbers from "./partialsMain/ProBarbers.vue";
 import ServicesPart from "./partialsMain/ServicesPart.vue";
-import GroomingProducts from "./partialsMain/GroomingProducts.vue"
-import ReviewsPart from "./partialsMain/ReviewsPart.vue"
+import GroomingProducts from "./partialsMain/GroomingProducts.vue";
+import ReviewsPart from "./partialsMain/ReviewsPart.vue";
+import RecentBlogPosts from "./partialsMain/RecentBlogPosts.vue";
 
 export default {
     name: "MyMain",
@@ -23,17 +25,8 @@ export default {
         ProBarbers,
         ServicesPart,
         GroomingProducts,
-        ReviewsPart
-    },
-
-    data(){
-        return{
-
-        }
-    },
-
-    methods: {
-        
+        ReviewsPart,
+        RecentBlogPosts
     }
 }
 </script>
---
+++ src/components/partialsMain/RecentBlogPosts.vue
@@ -0,0 +1,89 @@
+<template>
+
+    <div class="blog-post">
+
+        <div class="containerPartials">
+
+            <h4>tales from the barber shop</h4>
+            <h2>recent blog posts</h2>
+
+            <div class="display-containers">
+
+                <div class="containerPartials services-items">
+
+                    <img src="../../assets/img/avadabarbers-second-blog-700x441.jpg" alt="publicity">
+
+                    <h3>avada barbers now open</h3>
+
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur impedit optio vitae tempore, itaque dolorum. Ullam sunt blanditiis animi harum autem repellat qui eligendi, adipisci incidunt eos quibusdam? Aliquam, nemo.</p>
+
+                </div>
+
+                <div class="containerPartials services-items">
+
+                    <img src="../../assets/img/avadabarbers-choosing-blog-700x441.jpg" alt="publicity">
+
+                    <h3>chosing the right barber</h3>
+
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur impedit optio vitae tempore, itaque dolorum. Ullam sunt blanditiis animi harum autem repellat qui eligendi, adipisci incidunt eos quibusdam? Aliquam, nemo.</p>
+
+                </div>
+
+                <div class="containerPartials services-items">
+
+                    <img src="../../assets/img/avadabarbers-ourservice-blog-700x441.jpg" alt="publicity">
+
+                    <h3>quick service guide</h3>
+
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur impedit optio vitae tempore, itaque dolorum. Ullam sunt blanditiis animi harum autem repellat qui eligendi, adipisci incidunt eos quibusdam? Aliquam, nemo.</p>
+
+                </div>
+
+            </div>
+
+            <button>read more from our blog</button>
+
+        </div>
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "RecentBlogPosts"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/variables.scss";
+
+.blog-post {
+    background-color: #f5f5f5;
+
+    h2 {
+        margin: -15px 0 20px;
+    }
+
+    .services-items {
+    padding: 10px;
+    width: $width3;
+
+    img {
+        height: auto;
+        width: 100%;
+    }
+
+    h3 {
+        width: 90%;
+        font-size: 1.4rem;
+        text-align: center;
+    }
+    }
+
+    button {
+        margin-top: 50px;
+    }
+}
+
+</style>

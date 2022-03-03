f4fc2dd48fc6d09b823303b175e61dc556a5df7d
yuribertoli <yuribertoli@yahoo.it>
2022-03-03 16:35:11 +0100

    third part

--- src/components/partialsMain/ServicesPart.vue
+++ src/components/partialsMain/ServicesPart.vue
@@ -6,6 +6,41 @@
 
       <h4>mens grooming</h4>
       <h2>services</h2>
+
+      <div class="display-containers">
+
+        <div class="containerPartials services-items">
+
+          <img src="../../assets/img/avadabarbers-trimcut-icon-before.png" alt="trimcut">
+
+          <h3>trim &#38; cut</h3>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur impedit optio vitae tempore, itaque dolorum. Ullam sunt blanditiis animi harum autem repellat qui eligendi, adipisci incidunt eos quibusdam? Aliquam, nemo.</p>
+
+        </div>
+
+        <div class="containerPartials services-items">
+
+          <img src="../../assets/img/avadabarbers-washndry-icon.png" alt="barber soap">
+
+          <h3>wash &#38; dry</h3>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur impedit optio vitae tempore, itaque dolorum. Ullam sunt blanditiis animi harum autem repellat qui eligendi, adipisci incidunt eos quibusdam? Aliquam, nemo.</p>
+
+        </div>
+
+        <div class="containerPartials services-items">
+
+          <img src="../../assets/img/avadabarbers-beardtrim-icon.png" alt="beard trimmer">
+
+          <h3>beard tidy</h3>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur impedit optio vitae tempore, itaque dolorum. Ullam sunt blanditiis animi harum autem repellat qui eligendi, adipisci incidunt eos quibusdam? Aliquam, nemo.</p>
+
+        </div>
+
+      </div>
+
       <button>read about our services</button>
 
     </div>
@@ -28,6 +63,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../../assets/style/variables.scss";
+
+.container-partials {
+  width: $width3;
+}
+
+.services-items {
+  padding: 50px;
+
+  img {
+    height: 60px;
+    width: auto;
+  }
+}
+
 
 </style>
